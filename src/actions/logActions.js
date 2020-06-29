cfbd060a5d4224a67ed4485d8def19ef4551eb8d
Mayank Rustagi <mayankrustagi11@gmail.com>
2020-06-29 16:46:51 +0530

    DeleteLog

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,10 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from './types';
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR,
+  ADD_LOG,
+  DELETE_LOG,
+} from './types';
 
 // GET LOGS FROM SERVER
 export const getLogs = () => async (dispatch) => {
@@ -17,7 +23,7 @@ export const addLog = (log) => async (dispatch) => {
   try {
     setLoading();
     const res = await fetch('/logs', {
-      method: 'post',
+      method: 'POST',
       body: JSON.stringify(log),
       headers: {
         'Content-Type': 'application/json',
@@ -25,6 +31,19 @@ export const addLog = (log) => async (dispatch) => {
     });
     const data = await res.json();
     dispatch({ type: ADD_LOG, payload: data });
+  } catch (err) {
+    dispatch({ type: LOGS_ERROR, payload: err.response.data });
+  }
+};
+
+// DELETE LOG
+export const deleteLog = (id) => async (dispatch) => {
+  try {
+    setLoading();
+    await fetch(`/logs/${id}`, {
+      method: 'DELETE',
+    });
+    dispatch({ type: DELETE_LOG, payload: id });
   } catch (err) {
     dispatch({ type: LOGS_ERROR, payload: err.response.data });
   }
