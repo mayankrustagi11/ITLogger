6e5f236c8e606cd9de036a8aeb5afa1836ff8729
Mayank Rustagi <mayankrustagi11@gmail.com>
2020-06-29 22:48:25 +0530

    DeleteTechnician

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -36,6 +36,19 @@ export const addTech = (tech) => async (dispatch) => {
   }
 };
 
+// ADD TECHNICIAN
+export const deleteTech = (id) => async (dispatch) => {
+  try {
+    setLoading();
+    await fetch(`/techs/${id}`, {
+      method: 'DELETE',
+    });
+    dispatch({ type: DELETE_TECH, payload: id });
+  } catch (err) {
+    dispatch({ type: TECHS_ERROR, payload: err.response.statusText });
+  }
+};
+
 // SET LOADING TO TRUE
 export const setLoading = () => {
   return {
