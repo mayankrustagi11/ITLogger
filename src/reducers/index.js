f8ebbb820ce590d442eb984ba74395bd16929ec4
Mayank Rustagi <mayankrustagi11@gmail.com>
2020-06-29 22:14:08 +0530

    GetTechs

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import LogReducer from './logReducer';
+import TechReducer from './techReducer';
 
 export default combineReducers({
   log: LogReducer,
+  tech: TechReducer,
 });
